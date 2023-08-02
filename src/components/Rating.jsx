af697ebe695c4209304af2ad65f3a8590d99027d
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-02 22:37:57 +0200

    Update Rating.jsx

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,22 +4,46 @@ import "../App.css";
 
 const rating = () => {
   const [count, setCount] = useState(0);
+
   return (
     <div class="container">
       <div class="card">
-        <img src={icon} class="icon" alt="icon" />
+        <div class="circle">
+          <img src={icon} class="icon" alt="icon" />
+        </div>
         <h1>How did we do ? </h1>
         <p>
           Please let us know how we did with your support request. All feedback
           is appreciated to help us improve our offering!
         </p>
-        <ul class="ratings">
-          <li> 1 </li>
-          <li> 2 </li>
-          <li> 3 </li>
-          <li> 4 </li>
-          <li> 5 </li>
-        </ul>
+        <div class="ratings">
+          <div class="circle">
+            <div class="one" value={1}>
+              1
+            </div>
+          </div>
+          <div class="circle">
+            <div class="two" value={2}>
+              2
+            </div>
+          </div>
+          <div class="circle">
+            <div class="three" value={3}>
+              3
+            </div>
+          </div>
+          <div class="circle">
+            <div class="four" value={4}>
+              4
+            </div>
+          </div>
+          <div class="circle">
+            <div class="five" value={5}>
+              5
+            </div>
+          </div>
+        </div>
+
         <button
           onClick={() => setCount((count) => count + 1)}
           class="submit-btn"
