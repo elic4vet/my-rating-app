c588c1d91bb2c7b229fb10c1ba6881a31c089e8d
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-02 22:37:50 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import { React, useState } from "react";
 import Rating from "./components/Rating.jsx";
+import Success from "./components/Success";
 
 function App() {
+  const [success, setSuccess] = useState(true);
+  const [email, setEmail] = useState("");
   return (
     <>
-      <Rating />
+      {success ? (
+        <Rating setSuccess={setSuccess} setEmail={setEmail} email={email} />
+      ) : (
+        <Success email={email} setSuccess={setSuccess} />
+      )}
     </>
   );
 }
