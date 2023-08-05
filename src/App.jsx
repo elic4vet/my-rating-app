a2ea642ad3e7cc614f976ed23a809d0a7b32ea22
elic4vet <86532060+elic4vet@users.noreply.github.com>
2023-08-05 13:05:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import Rating from "./components/Rating.jsx";
 import "./App.css";
 
 function App() {
-  const [success, setSuccess] = useState(true);
-  const [selectedNumber, setSelectedNumber] = useState(0);
-
   return (
     <>
       <Rating />
